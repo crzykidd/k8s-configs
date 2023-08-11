94c3aad43b7f5085c028c062a706ee2203603f06
Matthew Andersen <mander@loginet.com>
2023-08-10 20:00:03 -0700

    updates to backup script

--- scripts/backuppods.sh
+++ scripts/backuppods.sh
@@ -1,24 +1,27 @@
-#/bin/bash
-# script backups directory from pod.  Runs once per week and stores the copy in a backup dir based on the date and hour
-# You can set the number of DAYS_TO_KEEP below.   If you run it more than once an hour it will overwrite.
-CURRENT_TIME=$(date +"%Y%m%d-%H")
-DAYS_TO_KEEP=31
+#! /bin/bash
+current_time=$(date +"%Y%m%d-%H")
+# Cleanup file for csv
+# Get URL from Secrets file
+url=$(sed '/discordUrlBackup/ s/[^:]*: *//; s/,.*//' .secret-scripts.yaml)
 
-# Settings for each job
-SONARR_GETPOD=$(kubectl get pods -lapp=sonarr-ui -n dnloads --output jsonpath='{.items[0].metadata.name}')
-SONARR_BACKUPPATH=/mnt/k8s-configs/backups/dnloads/sonarr
-SONARR_BACKUPDIR=/config
-RADARR_GETPOD=$(kubectl get pods -lapp=radarr-ui -n dnloads --output jsonpath='{.items[0].metadata.name}')
-RADARR_BACKUPPATH=/mnt/k8s-configs/backups/dnloads/radarr
-RADARR_BACKUPDIR=/config
+filecontents=$(sed '1d;s/.$//' backuppods.csv)
+lastjobseconds=0
+totaljobseconds=0
+message="Backup of k8s cluster containers started \\n"
+while IFS="," read -r podname namespace backupto backupfrom daystokeep
+do
+  getcontainername=$(kubectl get pods -lapp=$podname -n $namespace --output jsonpath='{.items[0].metadata.name}')
+  #echo "getcontainername: $getcontainername"
+  kubectl cp $getcontainername:$backupfrom $backupto/$podname/$current_time/ -n $namespace
+  totalseconds=`echo $SECONDS`
+  lastjobseconds=$((totalseconds-totaljobseconds))
+  totaljobseconds=$((totaljobseconds+lastjobseconds))
+  echo "Backup of $getcontainername took $lastjobseconds seconds"
+  message+="Backup of $getcontainername took $lastjobseconds seconds\\n"
+  find $backupto/$podname/* -maxdepth 0  -type d -ctime +$daystokeep -exec echo {} \;
+done <<< "$filecontents"
 
+totalseconds=`echo $SECONDS`
+message+="Backup of k8s cluster complete and took $totalseconds seconds"
 
-#Backup Sonarr
-kubectl cp $SONARR_GETPOD:$SONARR_BACKUPDIR $SONARR_BACKUPPATH/$CURRENT_TIME/ -n dnloads
-#Remove old backups from Sonarr
-find $SONARR_BACKUPPATH/* -maxdepth 0  -type d -ctime +$DAYS_TO_KEEP -exec rm -rf {} \;
-
-#Backup Radarr
-kubectl cp $RADARR_GETPOD:$RADARR_BACKUPDIR $RADARR_BACKUPPATH/$CURRENT_TIME/ -n dnloads
-#Remove old backups from Radarr
-find $RADARR_BACKUPPATH/* -maxdepth 0  -type d -ctime +$DAYS_TO_KEEP -exec rm -rf {} \;
+curl -H "Content-Type: application/json" -X POST -d '{"username": "k3s-c01", "content":"'"${message}"'"}'  $url
